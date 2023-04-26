9bcbf7e743b964c3bbc38f0f108b203e74976bc9
Richard Bentil <richard.bentil@peswa.finance>
2023-04-26 07:34:37 +0000

    select random question

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import questions from "../questions.json";
+
+interface QuestionProps {
+  id: number;
+  question: string;
+  answers: any[];
+  correctAnswer: string;
+}
 
 function Home() {
-  const [question, setquestion] = useState(null);
+  const [question, setquestion] = useState<QuestionProps>({
+    id: 0,
+    question: "",
+    answers: [],
+    correctAnswer: "",
+  });
   const [answer, setanswer] = useState("");
   const [score, setscore] = useState(0);
+
+  //set random questions
+  useEffect(() => {
+    const index = Math.floor(Math.random() * questions?.quizQuestions.length);
+    const quest: QuestionProps = questions?.quizQuestions[index];
+    if (quest?.id) {
+      setquestion(quest);
+    }
+    console.log(index, quest);
+  }, []);
 
   const onSubmit = (e: any) => {
     e.preventDefault();
     console.log(answer);
 
-    setscore((prevScore: number) => prevScore + 10)
-   
+    setscore((prevScore: number) => prevScore + 10);
   };
 
-  
   return (
     <div>
       <div>
         <p>Question</p>
         <form action="" onSubmit={onSubmit}>
-          {question}
+          {question?.id}
           <label htmlFor="me">
             <input
               type="radio"
@@ -28,7 +49,7 @@ function Home() {
               value={"me"}
               onChange={(e) => setanswer(e.target.value)}
             />
-            option 1
+             {question?.question}
           </label>
           <button type="submit">Submit</button>
         </form>
