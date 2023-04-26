695aaecf1d11405ce0a5ff8334d6908333e9355a
Richard Bentil <richard.bentil@peswa.finance>
2023-04-26 07:14:14 +0000

    setup handleanswer and score

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Home() {
+  const [question, setquestion] = useState(null);
+  const [answer, setanswer] = useState("");
+  const [score, setscore] = useState(0);
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(answer);
+
+    setscore((prevScore: number) => prevScore + 10)
+   
+  };
+
+  
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div>
+        <p>Question</p>
+        <form action="" onSubmit={onSubmit}>
+          {question}
+          <label htmlFor="me">
+            <input
+              type="radio"
+              name="answers"
+              id="me"
+              value={"me"}
+              onChange={(e) => setanswer(e.target.value)}
+            />
+            option 1
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
